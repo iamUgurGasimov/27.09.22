3b4b0c594741100f2e2e97360e2140b8a6eb3cbf
iamUgurGasimov <ughurvg@code.edu.az>
2022-09-28 07:44:27 +0400

    task about class

---
+++ task (class)/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task__class_
+{
+    class Engineer
+    {
+        public string name;
+        public string surname;
+        public int age;
+        public int grade;
+        public string address;
+
+        public Engineer()
+        {
+
+        }
+
+        public Engineer(string n, string s)
+        {
+            name = n;
+            surname = s;    
+
+        }
+
+        public Engineer(string n, string s,int a):this(n,s)
+        {
+            
+            age = a;
+            
+
+        }
+
+
+
+        public string Info()
+        {
+             
+            return $"name: {name} surname: {surname}";
+        }
+
+        public string FullInfo()
+        {
+            return $"{Info()} age : {age} grade : {grade} address : {address}";
+        }
+    }
+}
---
+++ task (class)/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace task__class_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+              
+            Engineer eng1=new Engineer();
+            eng1.name = "Ugur";
+            eng1.surname = "Gasimov";
+            eng1.age = 24;
+            eng1.grade = 78;
+            eng1.address = "Baku";
+            
+
+
+
+
+            Engineer eng2 = new Engineer();
+            eng2.name = "Sabir";
+            eng2.surname = "Gasimov";
+            eng2.age = 29;
+            eng2.grade = 84;
+            eng2.address = "Baku";
+
+
+            Engineer eng3 = new Engineer("Elmin","Gasimov",27);
+            eng3.grade = 91;
+            eng3.address = "Gence";
+            Console.WriteLine(eng3.FullInfo());
+            
+
+
+
+        }
+    }
+}       
+
+           
